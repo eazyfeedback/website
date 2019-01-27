c556a6e6f35165e226712f2accec5d93aa054ab9
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2019-01-27 02:00:24 -0800

    login lougout refactor  checkpoint

--- pages/_app.js
+++ pages/_app.js
@@ -4,13 +4,55 @@ import { MuiThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import JssProvider from "react-jss/lib/JssProvider";
 import getPageContext from "../lib/getPageContext";
-import Appbar from "../components/appbar";
 import Layout from "../components/layout";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import axios from "axios";
+import secrets from "../secrets";
 
 class MyApp extends App {
   constructor() {
     super();
     this.pageContext = getPageContext();
+    this.state = {
+      user: this.props.user
+    };
+  }
+
+  handleLogin() {
+    firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  handleLogout() {
+    firebase.auth().signOut();
+  }
+
+  handleAuth(user) {
+    if (user) {
+      setUser(user);
+      return user
+        .getIdToken()
+        .then(token => {
+          return axios("/auth/login", {
+            method: "POST",
+            headers: new Headers({
+              "Content-Type": "application/json"
+            }),
+            credentials: "same-origin",
+            body: JSON.stringify({
+              token
+            })
+          });
+        })
+        .then(() => console.log("login successul"));
+    } else {
+      setUser(null);
+      axios("/auth/logout", {
+        method: "POST",
+        credentials: "same-origin"
+      }).then(() => console.log("logout successful"));
+    }
   }
 
   componentDidMount() {
@@ -19,6 +61,12 @@ class MyApp extends App {
     if (jssStyles && jssStyles.parentNode) {
       jssStyles.parentNode.removeChild(jssStyles);
     }
+    // if (!firebase.apps.length) {
+    firebase.initializeApp(secrets.firebase.client);
+    if (user) console.log(user);
+    else console.log("no user");
+    firebase.auth().onAuthStateChanged(handleAuth);
+    // }
   }
 
   render() {
@@ -38,7 +86,6 @@ class MyApp extends App {
             {/* Pass pageContext to the _document though the renderPage enhancer
                 to render collected styles on server-side. */}
             <div style={{ height: "100vh" }}>
-              <Appbar />
               <Layout>
                 <Component pageContext={this.pageContext} {...pageProps} />
               </Layout>
@@ -48,6 +95,17 @@ class MyApp extends App {
       </Container>
     );
   }
+
+  static getInitialProps({ req }) {
+    const user = req && req.session ? req.session.decodedToken : null;
+    return {
+      user
+    };
+  }
 }
+
+MyApp.propTypes = {
+  user: PropTypes.object.isRequired
+};
 
 export default MyApp;
--- pages/_error.js
+++ pages/_error.js
@@ -1,16 +1,14 @@
 import { Typography } from "@material-ui/core";
 import PropTypes from "prop-types";
 
-export default class Error extends React.Component {
-  static getInitialProps({ res, err }) {
-    const statusCode = res ? res.statusCode : err ? err.statusCode : null;
-    return { statusCode };
-  }
-
-  render() {
-    return <Typography>{this.props.statusCode ? `An error ${this.props.statusCode} occurred on server` : "An error occurred on client"}</Typography>;
-  }
+function Error({ statusCode }) {
+  return <Typography>{statusCode ? `An error ${statusCode} occurred on server` : "An error occurred on client"}</Typography>;
 }
+
+Error.getInitialProps = async function({ res, err }) {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : null;
+  return { statusCode };
+};
 
 Error.propTypes = {
   statusCode: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.object]).isRequired

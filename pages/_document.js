6c66cb44d64c29d37510d781a834a64bdbd85912
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2019-02-03 06:56:34 -0800

    added favicon

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ class MyDocument extends Document {
       <html lang="en" dir="ltr">
         <Head>
           <meta charSet="utf-8" />
+          <link rel="shortcut icon" type="image/png" href="/static/favicon.png" />
           {/* Use minimum-scale=1 to enable GPU rasterization */}
           <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />

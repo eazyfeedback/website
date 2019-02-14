e52ed93a9c4fda51ffc268ff83b7ff70d8015d73
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2019-02-14 12:19:22 -0800

    rewrote APIEndpoint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  target: "serverless",
-  publicRuntimeConfig: {
-    APIEndpoint: process.env.NODE_ENV === "production" ? "https://essayfeedbackapi.now.sh/api" : "http://localhost:3001/api"
-  }
+  target: "serverless"
 };

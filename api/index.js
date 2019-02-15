418c4f335eff565b68301e80184acc935d3f4ebb
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2019-02-15 13:24:27 -0800

    checkpoint

--- api/index.js
+++ api/index.js
@@ -3,24 +3,74 @@ const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 80;
 const mongoose = require("mongoose");
 const cors = require("cors");
-const {
-  mongodb: { username, password }
-} = require("./secrets.json");
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const fileStore = require("session-file-store")(session);
+const admin = require("firebase-admin");
+const secrets = require("./secrets");
 
-mongoose.connect(`mongodb://${username}:${encodeURIComponent(password)}@ds161804.mlab.com:61804/essayfeedback`, { useNewUrlParser: true });
 const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(require("./logger"));
 app.use("/api/essays", require("./routes/essays"));
 app.use("/api/users", require("./routes/users"));
-app.listen(PORT, err => {
-  if (err) throw err;
-  console.info(`server ready at http://localhost:${PORT}`);
+
+const secret = secrets.firebase.secret;
+
+const firebase = admin.initializeApp(
+  {
+    credential: admin.credential.cert(secrets.firebase.server)
+  },
+  "server"
+);
+
+app.use(
+  session({
+    secret,
+    saveUninitialized: true,
+    store: new fileStore({ path: "/tmp/sessions", secret }),
+    resave: false,
+    rolling: true,
+    httpOnly: true,
+    cookie: { maxAge: 604800000 } // week
+  })
+);
+
+app.use((req, res, next) => {
+  req.firebaseServer = firebase;
+  next();
+});
+
+const {
+  mongodb: { username, password }
+} = secrets;
+mongoose.connect(`mongodb://${username}:${encodeURIComponent(password)}@ds161804.mlab.com:61804/essayfeedback`, { useNewUrlParser: true });
+
+app.post("/auth/login", (req, res) => {
+  if (!req.body) return res.sendStatus(400);
+  const token = req.body.token || "";
+  firebase
+    .auth()
+    .verifyIdToken(token)
+    .then(decodedToken => {
+      req.session.decodedToken = decodedToken;
+      return decodedToken;
+    })
+    .then(decodedToken => res.json({ status: true, decodedToken }))
+    .catch(error => res.json({ error }));
+});
+
+app.post("/auth/logout", (req, res) => {
+  req.session.decodedToken = null;
+  res.json({ status: true });
 });
 
 mongoose.set("debug", true);
+
 mongoose.connection.on("connected", function() {
   console.info("Mongoose is connected");
 });
@@ -40,76 +90,7 @@ process.on("SIGINT", function() {
   });
 });
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const session = require("express-session");
-const fileStore = require("session-file-store")(session);
-const next = require("next");
-const admin = require("firebase-admin");
-const secrets = require("./secrets");
-
-const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
-
-const firebase = admin.initializeApp(
-  {
-    credential: admin.credential.cert(secrets.firebase.server)
-  },
-  "server"
-);
-
-app.prepare().then(() => {
-  const server = express();
-  server.use(bodyParser.json());
-  const secret = secrets.firebase.secret;
-  server.use(
-    session({
-      secret,
-      saveUninitialized: true,
-      store: new fileStore({ path: "/tmp/sessions", secret }),
-      resave: false,
-      rolling: true,
-      httpOnly: true,
-      cookie: { maxAge: 604800000 } // week
-    })
-  );
-
-  server.use((req, res, next) => {
-    req.firebaseServer = firebase;
-    next();
-  });
-
-  server.post("/auth/login", (req, res) => {
-    if (!req.body) return res.sendStatus(400);
-    const token = req.body.token || "";
-    firebase
-      .auth()
-      .verifyIdToken(token)
-      .then(decodedToken => {
-        req.session.decodedToken = decodedToken;
-        return decodedToken;
-      })
-      .then(decodedToken => res.json({ status: true, decodedToken }))
-      .catch(error => res.json({ error }));
-  });
-
-  server.post("/auth/logout", (req, res) => {
-    req.session.decodedToken = null;
-    res.json({ status: true });
-  });
-
-  server.get("*", (req, res) => {
-    return handle(req, res);
-  });
-
-  server.post("*", (req, res) => {
-    return handle(req, res);
-  });
-
-  server.listen(port, err => {
-    if (err) throw err;
-    console.log(`> Server on http://localhost:${port}`);
-  });
+app.listen(PORT, err => {
+  if (err) throw err;
+  console.info(`server ready at http://localhost:${PORT}`);
 });

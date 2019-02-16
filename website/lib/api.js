5d5fc97b1e399ede6d2f95e50899fb20ca9ab2f7
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2019-02-15 22:40:51 -0800

    checkpoint

--- website/lib/api.js
+++ website/lib/api.js
@@ -1,2 +1,2 @@
-const APIEndpoint = process.env.NODE_ENV === "production" ? "https://essayfeedback.now.sh:3001/api" : "http://localhost:3001/api";
+const APIEndpoint = process.env.NODE_ENV === "production" ? "https://essayfeedback.now.sh/api" : "http://localhost:3001/api";
 export default APIEndpoint;
